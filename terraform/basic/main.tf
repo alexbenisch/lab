5d84b08f4d33e04ef0b1f1dd3a1ae9f6de561095
alexbenisch <alexander.benisch@gmail.com>
2025-04-05 11:21:14 +0200

    updated files, removed setup.sh

--- terraform/basic/main.tf
+++ terraform/basic/main.tf
@@ -2,63 +2,73 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "~>1.44"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "~>3.0"
+      version = "~> 1.44"
     }
   }
 }
 
+
 provider "hcloud" {
   token = var.hcloud_token
+
 }
-
-provider "http" {}
-
-data "http" "github_keys" {
-  url = "https://github.com/${var.github_username}.keys"
+resource "hcloud_ssh_key" "alex" {
+  name       = "alex-desktop"
+  public_key = file("~/.ssh/id_ed25519.pub") # oder direkt als Variable
 }
-
-locals {
-  github_ssh_keys = split("\n", trimspace(data.http.github_keys.response_body))
-}
-
-output "ssh_keys" {
-  value = local.github_ssh_keys
-}
-
-resource "hcloud_ssh_key" "github" {
-  name       = "github-key"
-  public_key = local.github_ssh_keys[0]
-}
-
-resource "hcloud_server" "machine" {
-  name        = "machine"
+resource "hcloud_server" "basic" {
+  name        = "basic"
   image       = "debian-12"
   server_type = "cpx11"
   location    = "fsn1"
-  ssh_keys    = [hcloud_ssh_key.github.id]
-
+  ssh_keys    = [hcloud_ssh_key.alex.id] # Statt var.ssh_key
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
   }
+
+  user_data = <<-EOF
+    #cloud-config
+    package_update: true
+    package_upgrade: true
+    packages:
+      - zsh
+      - stow
+      - tmux
+      - fzf
+      - ripgrep
+      - git
+
+    users:
+      - name: alex
+        groups: ["sudo","docker"]
+        shell: /bin/zsh
+        sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+        ssh-authorized-keys:
+          - ${var.ssh_key}
+        lock_passwd: false
+        passwd: "$6$rounds=4096$XJ5p3uJ5HUE68...hashedpassword..."  # Verschlüsseltes Passwort
+
+    runcmd:
+      - sudo -u alex git clone https://github.com/alexbenisch/dotfiles.git /home/alex/.local/share/dotfiles
+      - chsh -s $(which zsh) alex
+      - bash /home/alex/.local/share/dotfiles/setup.sh
+  EOF
+}
+resource "time_sleep" "wait_for_ip" {
+  depends_on = [hcloud_server.basic]
+
+  create_duration = "30s" # Wartezeit (anpassen je nach Hetzner Geschwindigkeit)
 }
 
-variable "github_username" {
-  description = "GitHub Username für SSH Public Keys"
-  type        = string
+output "server_ip" {
+  depends_on = [time_sleep.wait_for_ip]
+  value      = hcloud_server.basic.ipv4_address
 }
+variable "hcloud_token" {}
+variable "ssh_key" {}
 
-variable "ssh_keys" {
-  description = "Liste der SSH Public Keys"
-  type        = list(string)
-  default     = []
-}
 
-variable "hcloud_token" {
-  description = "Hetzner Cloud API Token"
-  type        = string
-}
+
+
+
